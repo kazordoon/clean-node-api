b17ff751e6e10443d05255a4d9aee946c5c82f68
Felipe Barros <felipetech@tuta.io>
2020-05-31 20:23:22 -0300

    refactor: Move production code to their own files

---
+++ src/presentation/helpers/HttpResponse.js
@@ -0,0 +1,18 @@
+const MissingParamError = require('./MissingParamError')
+
+class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static internalServerError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
+
+module.exports = HttpResponse
---
+++ src/presentation/routes/LoginRouter.js
@@ -0,0 +1,20 @@
+const HttpResponse = require('../helpers/HttpResponse')
+
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.internalServerError()
+    }
+
+    const { email, password } = httpRequest.body
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+  }
+}
+
+module.exports = LoginRouter
