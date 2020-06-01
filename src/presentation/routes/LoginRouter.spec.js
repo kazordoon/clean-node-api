70f1ba633fe77ad35ae96a43bed51e5780632134
Felipe Barros <felipetech@tuta.io>
2020-05-31 21:05:03 -0300

    refactor: Move SUT creation
    Move SUT creation to a factory helper method

--- src/presentation/routes/LoginRouter.spec.js
+++ src/presentation/routes/LoginRouter.spec.js
@@ -1,9 +1,13 @@
 const LoginRouter = require('./LoginRouter.js')
 const MissingParamError = require('../helpers/MissingParamError')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
+
 describe('Login router', () => {
   it('should return status code 400 if no email is provided', () => {
-    const SUT = new LoginRouter()
+    const SUT = makeSut()
     const httpRequest = {
       body: {
         password: 'mypass'
@@ -16,7 +20,7 @@ describe('Login router', () => {
   })
 
   it('should return status code 400 if no password is provided', () => {
-    const SUT = new LoginRouter()
+    const SUT = makeSut()
     const httpRequest = {
       body: {
         email: 'myemail@mail.com'
@@ -29,14 +33,14 @@ describe('Login router', () => {
   })
 
   it('should return status code 500 if no httpRequest is provided', () => {
-    const SUT = new LoginRouter()
+    const SUT = makeSut()
 
     const httpResponse = SUT.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   it('should return status code 500 if no httpRequest has no body', () => {
-    const SUT = new LoginRouter()
+    const SUT = makeSut()
 
     const httpRequest = {}
     const httpResponse = SUT.route(httpRequest)
