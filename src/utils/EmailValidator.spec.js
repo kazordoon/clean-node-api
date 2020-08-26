e45a410118755b2e721ac469182bc74abd08c188
Felipe Barros <felipetech@tuta.io>
2020-08-26 11:05:13 -0300

    refactor: Move SUT creation to a factory helper method

--- src/utils/EmailValidator.spec.js
+++ src/utils/EmailValidator.spec.js
@@ -6,9 +6,13 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   it('should return true if validator returns true', () => {
-    const SUT = new EmailValidator()
+    const SUT = makeSut()
     const isEmailValid = SUT.isValid('valid_email@mail.com')
 
     expect(isEmailValid).toBe(true)
@@ -16,7 +20,7 @@ describe('Email Validator', () => {
 
   it('should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const SUT = new EmailValidator()
+    const SUT = makeSut()
     const isEmailValid = SUT.isValid('invalid_email@mail.com')
 
     expect(isEmailValid).toBe(false)
