ebd3bae6d2bed5476aee2a615ae45b2c921c966f
Felipe Barros <felipetech@tuta.io>
2020-08-26 11:10:29 -0300

    refactor: Move EmailValidator to its own file

--- src/utils/EmailValidator.spec.js
+++ src/utils/EmailValidator.spec.js
@@ -1,10 +1,5 @@
 const validator = require('validator')
-
-class EmailValidator {
-  isValid (email) {
-    return validator.isEmail(email)
-  }
-}
+const EmailValidator = require('./EmailValidator')
 
 const makeSut = () => {
   return new EmailValidator()
