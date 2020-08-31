ce5eda5126ef458a6639d0259fe5608106192569
Felipe Barros <felipetech@tuta.io>
2020-08-31 19:35:10 -0300

    refactor: Move Encrypter to its own file

--- src/utils/Encrypter.spec.js
+++ src/utils/Encrypter.spec.js
@@ -1,15 +1,14 @@
 const bcrypt = require('bcrypt')
+const Encrypter = require('./Encrypter')
 
-class Encrypter {
-  async compare (value, hash) {
-    const isValid = await bcrypt.compare(value, hash)
-    return isValid
-  }
+const makeSUT = () => {
+  const encrypter = new Encrypter()
+  return encrypter
 }
 
 describe('Encrypter', () => {
   it('should return true if bcrypt returns true', async () => {
-    const SUT = new Encrypter()
+    const SUT = makeSUT()
 
     const isValid = await SUT.compare('any_value', 'hashed_value')
 
@@ -17,10 +16,19 @@ describe('Encrypter', () => {
   })
 
   it('should return false if bcrypt returns false', async () => {
-    const SUT = new Encrypter()
+    const SUT = makeSUT()
     bcrypt.isValid = false
     const isValid = await SUT.compare('any_value', 'hashed_value')
 
     expect(isValid).toBe(false)
+  })
+
+  it('should call bcrypt with correct values', async () => {
+    const SUT = makeSUT()
+
+    await SUT.compare('any_value', 'hashed_value')
+
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })
