d51ea46589ca44f66725e9def6886d900332f26c
Felipe Barros <felipetech@tuta.io>
2020-09-01 20:10:10 -0300

    refactor: Move SUT creation to a factory helper method

--- src/utils/TokenGenerator.spec.js
+++ src/utils/TokenGenerator.spec.js
@@ -1,14 +1,28 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
   async generate (id) {
-    return null
+    return jwt.sign(id, 'secret_key')
   }
+}
+
+const makeSUT = () => {
+  return new TokenGenerator()
 }
 
 describe('TokenGenerator', () => {
   it('should return null if JWT returns null', async () => {
-    const SUT = new TokenGenerator()
+    const SUT = makeSUT()
+    jwt.token = null
     const token = await SUT.generate('any_id')
 
     expect(token).toBeNull()
+  })
+
+  it('should return token if JWT returns token', async () => {
+    const SUT = makeSUT()
+    const token = await SUT.generate('any_id')
+
+    expect(token).toBe(jwt.token)
   })
 })
