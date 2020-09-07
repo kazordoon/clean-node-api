75d106da76bb236102fdb020b1f3ae84c3e81183
Felipe Barros <felipetech@tuta.io>
2020-09-07 13:57:48 -0300

    refactor: Move test route to its own test file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,3 @@
 const app = require('fastify')()
 
-app.get('/', (request, reply) => {
-  return { hello: 'world' }
-})
-
 module.exports = app
--- src/main/config/app.test.js
+++ src/main/config/app.test.js
@@ -3,6 +3,10 @@ const app = require('./app')
 describe('App', () => {
   describe('GET /', () => {
     it('should return status code 200', async () => {
+      app.get('/', (request, reply) => {
+        return reply.code(200).send()
+      })
+
       const response = await app.inject({
         method: 'GET',
         url: '/'
