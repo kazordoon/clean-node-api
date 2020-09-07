a6931499e2224f850caa357e19790fc65ccbf10c
Felipe Barros <felipetech@tuta.io>
2020-09-07 14:26:28 -0300

    test: Ensure the CORS is enabled

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,3 +1,10 @@
 const app = require('fastify')()
+const cors = require('fastify-cors')
+
+app.register(cors, {
+  origin: '*',
+  methods: '*',
+  allowedHeaders: '*'
+})
 
 module.exports = app
--- src/main/config/app.test.js
+++ src/main/config/app.test.js
@@ -1,18 +1,18 @@
 const app = require('./app')
 
 describe('App', () => {
-  describe('GET /', () => {
-    it('should return status code 200', async () => {
-      app.get('/', (request, reply) => {
-        return reply.code(200).send()
-      })
-
-      const response = await app.inject({
-        method: 'GET',
-        url: '/'
-      })
-
-      expect(response.statusCode).toBe(200)
+  it('should enable CORS', async () => {
+    app.options('/test_cors', (request, reply) => {
+      return reply.code(200).send()
     })
+
+    const response = await app.inject({
+      method: 'OPTIONS',
+      url: '/test_cors'
+    })
+
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
   })
 })
